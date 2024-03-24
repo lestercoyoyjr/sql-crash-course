31ccc7c6a30e6749741f076debd537aa17c187d7
lestercoyoy <lester_cch@hotmail.com>
2024-03-23 23:38:19 -0600

    ADD select queries examples

---
+++ select_queries.sql
@@ -0,0 +1,9 @@
+SELECT * FROM customer;
+    
+SELECT CustomerID FROM customer;
+
+SELECT CustomerID, Name FROM customer;
+
+SELECT CustomerID, Name, City FROM customer;
+
+SELECT CustomerID, Name, City FROM customer LIMIT 2;
