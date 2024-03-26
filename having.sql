26d1f9733baa99a8162399182cb6854cf6495be4
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2024-03-26 00:10:42 -0600

    MODIFY query to get continent and LifeExpectancy relation

--- having.sql
+++ having.sql
@@ -7,3 +7,5 @@ SELECT MAX(LifeExpectancy), Name FROM Country GROUP BY Name HAVING MAX(LifeExpec
 SELECT MAX(LifeExpectancy), Name FROM Country GROUP BY Name HAVING MAX(LifeExpectancy) > 80;
 
 SELECT LifeExpectancy, Name FROM Country WHERE LifeExpectancy > 80 ORDER BY LifeExpectancy ASC;
+
+SELECT LifeExpectancy, Name, Continent FROM Country WHERE LifeExpectancy > 78 ORDER BY LifeExpectancy DESC;
