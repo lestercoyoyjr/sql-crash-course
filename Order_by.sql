a26e1a989ea3d39407b3e871b47e5f0bfce4dabd
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2024-03-27 13:38:00 -0600

    MODIFY adding more SELECT examples for future references

--- Order_by.sql
+++ Order_by.sql
@@ -11,3 +11,7 @@ SELECT * FROM Country ORDER BY SurfaceArea;
 SELECT * FROM Country ORDER BY Name ASC;
 
 SELECT * FROM Country WHERE Continent = 'North America' AND NOT LifeExpectancy >= 70 ORDER BY LifeExpectancy DESC;
+
+SELECT Code,Name,Continent,Region,SurfaceArea FROM Country WHERE SurfaceArea > 1000000 ORDER BY SurfaceArea DESC;
+
+SELECT Name, LifeExpectancy AS LE FROM Country WHERE LifeExpectancy IS NOT NULL ORDER BY LifeExpectancy ASC LIMIT 10;
